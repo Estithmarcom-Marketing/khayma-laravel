44b61cefb9a53ac4e50cea9a5c574ca836d7713b
Mhmud-Elsayed <houdaaaelsayed22222@gmail.com>
2026-02-18 16:33:51 +0200

    remove images of subcategories response

--- app/Services/V1/Website/Category/CategoryService.php
+++ app/Services/V1/Website/Category/CategoryService.php
@@ -10,7 +10,7 @@ class CategoryService
     {
         return Category::select(['id', 'name_ar', 'name_en'])
             ->with([
-                'subCategories.media:id,model_id,name,file_name,collection_name,disk'])
+                'subCategories:id,name_ar,name_en,parent_id'])
             ->whereNull('parent_id')
             ->get();
     }
@@ -19,7 +19,7 @@ class CategoryService
     {
         return Category::select(['id', 'name_ar', 'name_en'])
             ->with([
-                'subCategories.media:id,model_id,name,file_name,collection_name,disk'])
+                'subCategories:id,name_ar,name_en,parent_id'])
             ->findorFail($id);
     }
 }
