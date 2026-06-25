c33adee3b2e0262b7278e40c197d3bc589035716
Mhmud-Elsayed <houdaaaelsayed22222@gmail.com>
2026-06-25 16:40:39 +0300

    update admin category resource

--- app/Http/Resources/Category/CategoryResource.php
+++ app/Http/Resources/Category/CategoryResource.php
@@ -39,7 +39,7 @@ class CategoryResource extends JsonResource
                     ];
                 });
             }),
-
+            'parent_id' => $this->parent_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
